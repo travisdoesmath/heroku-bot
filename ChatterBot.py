fc332868c8d7aebd80e4ed563b9fa3177ef3d017
Travis Taylor <travisdoesmath@gmail.com>
2018-03-12 19:22:28 -0500

    Initial Commit

--- ChatterBot.py
+++ ChatterBot.py
@@ -10,6 +10,7 @@ consumer_secret = os.getenv("consumer_secret")
 access_token = os.getenv("access_token")
 access_token_secret = os.getenv("access_token_secret")
 
+#pointless edit
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
